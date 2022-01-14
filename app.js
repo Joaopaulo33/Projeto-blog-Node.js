c788bc47797748cec430e2f0faa00155248c42ca
João paulo C <joaopaulo33573@gmail.com>
2022-01-14 18:56:23 -0300

    Configuração de rotas

--- app.js
+++ app.js
@@ -5,24 +5,54 @@ const app = express();
 const path = require("path");
 const admin = require("./routes/admin");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser')
+const session = require('express-session');
+const flash = require("connect-flash")
 
 //Configurações
+    //Session
+    app.use(session({
+        secret: "cursodenode",
+        resave: true,
+        saveUninitialized: true
+    }))
+
+    app.use(flash())
+
+    //Middleware
+        app.use((req, res, next)=>{
+            res.locals.success_msg = req.flash('success_msg')
+            res.locals.error_msg = req.flash('error_msg')
+            next();
+        })
+
     //handlebars
 const hbs = handlebars.create({
-    defaultLayout: "main";
+    defaultLayout: "main"
 })
 app.engine("handlebars", hbs.engine);
 app.set("view engine", 'handlebars');
 
-//bodyparse(no lugar dele)      
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+//bodyparse(no lugar dele)     
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())  
+// app.use(express.urlencoded({extended:false}));
+// app.use(express.json());
 
 
 //Mongoose
     //em breve
+    mongoose.Promise = global.Promise;
+    mongoose.connect("mongodb://127.0.0.1/blogapp",{
+       // useMongoClient: true //disse que não é suportado
+    }).then(()=>{
+        console.log("Conectado ao mongo");
+    }).catch((err) =>{
+        console.log("Erro ao se conctar:",err);
+    })
 
 //Public
+//Definindo que os arquivos estáticos vão ficar na pasta public
     app.use(express.static(path.join(__dirname,"public")));
 
 //Rotas
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+require("../models/Categoria")
+const Categoria = mongoose.model("categorias");
 
 router.get('/', (req,res)=>{
    res.render("admin/index");
@@ -16,6 +19,41 @@ router.get('/categorias',(req,res)=>{
 router.get('/categorias/add',(req,res)=>{
     res.render("admin/addcategorias");
 })
+
+router.post("/categorias/nova",(req,res)=>{
+
+    var erros=[]
+
+    if(!req.body.nome || typeof req.body.slug == undefined || req.body.nome == null){
+         erros.push({texto:"Nome inválido."}) 
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug === null){
+        erros.push({texto:"Slug inválido."})
+    }
+
+    if(req.body.nome.length<2){
+        erros.push({texto:"Nome da categoria é muito pequeno"})
+
+    }
+    if(erros.length>0){
+        res.render("admin/addcategorias",{erros : erros})
+    }else{
+        const novaCategoria = {
+        
+        nome: req.body.nome,
+        slug: req.body.slug
+}
+        new Categoria(novaCategoria).save().then(()=>{
+            console.log("Categoria salva com sucesso");
+        }).catch((err) =>{
+            console.log("Erro ao salvar categoria");
+        })
+    }
+
+    
+})
+
 
 
 module.exports = router;
